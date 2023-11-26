1e7c4c7f86655ba65ce4ecf845a28dc838021604
Vivek Tyagi <vvktyagi@gmail.com>
2023-11-27 00:06:21 +0530

    Initial 3 Recommender systems created

---
+++ HousingAdvisorWeb/pages/3_Recommender System.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="Society Recommender System")
+st.title('Society Recommender System')
+
+
+# st.dataframe(df)
+st.header("Reommended Societies")
